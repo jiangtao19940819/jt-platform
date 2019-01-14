246db7c3738860206bd4a7dc8fbedc6038a3f887
jiangtao <jiangtao@baiwang.com>
2019-01-14 23:58:47 +0800

    里程碑

--- src/components/auto_api/project.vue
+++ src/components/auto_api/project.vue
@@ -18,11 +18,35 @@
 				</template>
 				<template slot-scope="scope">
 					<el-button @click="editProject(scope.row)" type="success">Edit</el-button>
-					<el-button @click="delProject" type="danger">Delete</el-button>
+					<el-button @click="deleteProject(scope.row)" type="danger">Delete</el-button>
 				</template>
 			</el-table-column>
 			
 		</el-table>
+		<el-dialog title="是否删除" :visible.sync="dialogTableVisible" width="30%">
+			<span>这是一个提示</span>
+			<span slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="ensureDelProject">确 定</el-button>
+   			  	<el-button @click="dialogTableVisible = false">取 消</el-button>
+  			</span>
+		</el-dialog>
+		<el-dialog title="编辑项目" :visible.sync="dialogEditProject">
+			<el-form :model="project">
+				<el-form-item label="名称">
+					<el-input v-model="project.name" placeholder="project.name"></el-input>
+				</el-form-item>
+				<el-form-item label="时间">
+					<el-input v-model="project.time" placeholder="project.time"></el-input>
+				</el-form-item>
+				<el-form-item label="备注">
+					<el-input v-model="project.description" placeholder="project.description"></el-input>
+				</el-form-item>
+			</el-form>
+			<span slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="ensurEditProject">确 定</el-button>
+   			  	<el-button @click="dialogEditProject = false">取 消</el-button>
+  			</span>
+		</el-dialog>
 	</div>
 </template>
 <script>
@@ -64,13 +88,38 @@
 						description: '数据组接口'
        				}
 				],
-				search:""
+				project:{
+					name: '',
+					time: '',
+					description: ''
+				},
+				search:"",
+				dialogTableVisible:false,
+				dialogEditProject:false
 				}
 			},
 			methods: {
 				editProject(row){
-					alert(row);
+					this.project.name = row.name;
+					this.project.time = row.time;
+					this.project.description = row.description;
+					this.dialogEditProject = true;
+				},
+				ensurEditProject(){
+					console.log(this.project.name)
+					this.dialogEditProject = false;
+				},
+				deleteProject(row){
+					this.project.name = row.name;
+					this.project.time = row.time;
+					this.project.description = row.description;
+					this.dialogTableVisible = true;
+				},
+				ensureDelProject(){
+					console.log(this.project.name)
+					this.dialogTableVisible = false;
 				}
+
 			}
 		}
 </script>
