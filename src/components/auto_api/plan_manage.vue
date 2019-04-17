4e4f2d152f69f355e12a4c39e4934ea104a284a9
jiangtao <jiangtao@baiwang.com>
2019-04-18 00:41:38 +0800

    222222222222

--- src/components/auto_api/plan_manage.vue
+++ src/components/auto_api/plan_manage.vue
@@ -27,17 +27,52 @@
 					<el-table-column prop="baseUrl" label="请求网址"></el-table-column>
 					<el-table-column prop="userName" label="登录用户名" ></el-table-column>
 					<el-table-column prop="passWord" label="登录密码" ></el-table-column>
+					<el-table-column prop="email" label="收件人邮箱" ></el-table-column>
 					<el-table-column>
 						<template slot="header" slot-scope="scope">操作</template>
 						<template slot-scope="scope">
-							<el-button @click="editSuite(scope.row)" type="success" size="small">Edit</el-button>
-							<el-button @click="delSuite(scope.row)" type="danger" size="small">Delete</el-button>
+							<el-button @click="editPlan(scope.row)" type="success" size="small">Edit</el-button>
+							<el-button @click="delPlan(scope.row)" type="danger" size="small">Delete</el-button>
 						</template>
 					</el-table-column>
 			    </el-table>
 			</div>
 		</el-row>
-	
+		<el-dialog title="编辑计划" :visible.sync="dialogEditPlan">
+			<el-form :model="plan">
+				<el-form-item label="计划名称" :required="true">
+					<el-input v-model="plan.planName" placeholder="请输入计划名称"></el-input>
+				</el-form-item>
+				<el-form-item label="关联服务" :required="true">
+					<el-select v-model="plan.projectName">
+						<el-option v-for="item in projectList" :key="item" :value="item" :lable="item"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="请求网址" :required="true">
+					<el-input v-model="plan.baseUrl" placeholder="请输入请求网址"></el-input>
+				</el-form-item>
+				<el-form-item label="登录用户名">
+					<el-input v-model="plan.userName" placeholder="请输入登录用户名"></el-input>
+				</el-form-item>
+				<el-form-item label="请求网址">
+					<el-input v-model="plan.passWord" placeholder="请输入用户名密码"></el-input>
+				</el-form-item>
+				<el-form-item label="收件人邮箱">
+					<el-input v-model="plan.passWord" placeholder="请输入收件人邮箱"></el-input>
+				</el-form-item>
+			</el-form>
+			<span slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="ensureEditPlan">确定</el-button>
+				<el-button @click="dialogEditPlan=false">取消</el-button>
+			</span>
+		</el-dialog>
+		<el-dialog title="删除计划" :visible.sync="dialogDelPlan">
+			<span>请确认是否删除{{plan.planName}}</span>
+			<span slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="ensureDeletePlan">确认</el-button>
+				<el-button @click="dialogDelPlan=false">取消</el-button>
+			</span>
+		</el-dialog>
 	</div>
 		
 </template>
@@ -53,6 +88,22 @@
 				projectList:[],
 				projectName:"",
 				planName:"",
+				dialogEditPlan:false,
+				dialogDelPlan:false,
+				plan:{
+					planId:0,
+					planName:"",
+					projectName:"",
+					baseUrl:"",
+					userName:"",
+					passWord:"",
+					email:""
+				},
+				response:{
+					result:"",
+					msg:"",
+					data:[]
+				},
 				planData : [
 				]
 			}
@@ -67,6 +118,47 @@
 				this.$http.get("plan/queryPlan",{projectName:this.projectName,planName:this.planName}).then(res=>{
 					this.planData = res.data;
 				});
+			},
+			editPlan:function(row){
+				this.dialogEditPlan = true;
+				this.plan.planId = row.planId;
+				this.plan.planName = row.planName;
+				this.plan.projectName = row.projectName;
+				this.plan.baseUrl = row.baseUrl;
+				this.paln.userName = row.userName;
+				this.plan.passWord = row.passWord;
+				this.plan.email = row.email;
+
+			},
+			ensureEditPlan:function(){
+				this.$http.post("plan/updatePlan",this.plan).then(res=>{
+					this.response = res;
+					this.dialogEditPlan = false;
+					//this.step = Object.assign({}, this.newStep);
+					this.afterOperate();
+					this.getAllPlan();
+				})
+			},
+			delPlan:function(row){
+				this.plan.planId = row.planId;
+				this.plan.planName = row.planName;
+				this.dialogDelPlan = true;
+			},
+			ensureDeletePlan:function(){
+				this.$http.post("plan/delPlan",{planId:this.plan.planId}).then(res=>{
+					this.response = res;
+					this.dialogDelPlan = false;
+					//this.step = Object.assign({}, this.newStep);
+					this.afterOperate();
+					this.getAllPlan();
+				})
+			},
+			afterOperate(){
+				if(this.response.result=="200"){
+					this.$message.success(this.response.msg)
+				}else{
+					this.$message.error(this.response.msg)
+				}
 			}
 			
 		}
